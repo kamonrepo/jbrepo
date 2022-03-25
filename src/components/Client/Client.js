adf2770423ec34642580b2f7f2c16d2e32eefa45
Marvin Bendicio <marvin.bendicio.ipi@prulifeuk.com.ph>
2022-03-25 08:33:27 +0800

    update for today

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TextField, Button, Typography, Paper, Select, MenuItem } from '@material-ui/core';
+import { TextField, Button, Typography, Paper, Select, MenuItem, InputLabel } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import { getGroups } from '../../actions/group';
 import { createClient } from '../../actions/client';
@@ -7,17 +7,17 @@ import useStyles from './styles';
 
 const Client = () => {
 
-    const groups = useSelector(state => state.groups)
-    const [clientData, setClientData] = useState({  group:'', name: '', contactNumber: '', package: '', dueDate: '', monthlyFee: '', address: '' });
-    const [group, setGroup] = useState('');
-    const dispatch = useDispatch();
-    const classes = useStyles();
-    const user = JSON.parse(localStorage.getItem('profile'));
+  const groups = useSelector(state => state.groups)
+  const [clientData, setClientData] = useState({  group:'', name: '', contactNumber: '', package: '', dueDate: '', monthlyFee: '', address: '' });
+  const [group, setGroup] = useState('');
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem('profile'));
 
-    useEffect(() => {
-      dispatch(getGroups());
+  useEffect(() => {
+    dispatch(getGroups());
 
-    }, [])
+  }, [])
 
   const handleOnchange = data => {
     setGroup(data);
@@ -44,29 +44,32 @@ const Client = () => {
     );
   }
 
-    return (     
-      <>        
-        <Paper className={classes.paper} elevation={6}>
-          <form className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-              <Typography className={classes.Header} variant="h6"> Add Client </Typography>
-              <Select className={classes.Select} fullWidth value={group} onChange={e => handleOnchange(e.target.value)}>
-                  {groups.map((data) => (
-                    <MenuItem key={data.id} value={data.name}>{data.name}</MenuItem>
-                  ))}
-              </Select>
-              <TextField name="name" variant="outlined" label="Registered name" fullWidth value={clientData.name} onChange={(e) => setClientData({...clientData, name: e.target.value})} />
-              <TextField name="contactNumber" variant="outlined" label="Contact Number" fullWidth multiline rows={4} value={clientData.contactNumber} onChange={(e) => setClientData({...clientData, contactNumber: e.target.value})}  />
-              <TextField name="package" variant="outlined" label="Package" fullWidth value={clientData.package} onChange={(e) => setClientData({ ...clientData, package: e.target.value})} />
-              <TextField name="dueDate" variant="outlined" label="Due Date" fullWidth value={clientData.dueDate} onChange={(e) => setClientData({ ...clientData, dueDate: e.target.value})}/>
-              <TextField name="monthlyFee" variant="outlined" label="Monthly Fee" fullWidth value={clientData.monthlyFee} onChange={(e) => setClientData({ ...clientData, monthlyFee: e.target.value})}/>
-              <TextField name="address" variant="outlined" label="Address" fullWidth value={clientData.address} onChange={(e) => setClientData({...clientData, address: e.target.value})} />
-            
-              <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth> Submit </Button>
-              <Button variant="contained" color="secondary" size="small" fullWidth> Clear </Button>
-        </form>
-      </Paper>
-      </>
-    )
+  return (     
+    <>        
+      <Paper className={classes.paper} elevation={6}>
+        <form onSubmit={handleSubmit}>
+            <Typography className={classes.Header} variant="h6"><b>ADD CLIENT</b></Typography>
+
+            <InputLabel id="demo-simple-select-standard-label"><b>&nbsp;assigned group</b></InputLabel>
+              <Select labelId="demo-simple-select-standard-label" id="demo-simple-select-standard" className={classes.Select} fullWidth value={group} onChange={e => handleOnchange(e.target.value)}>
+                {groups.map((data) => (
+                  <MenuItem key={data.id} value={data.name}>{data.name}</MenuItem>
+                ))}
+             </Select>
+
+            <TextField className={classes.textFields} name="name" variant="outlined" label="Registered name" fullWidth value={clientData.name} onChange={(e) => setClientData({...clientData, name: e.target.value})} />
+            <TextField className={classes.textFields} name="contactNumber" variant="outlined" label="Contact Number" fullWidth multiline rows={4} value={clientData.contactNumber} onChange={(e) => setClientData({...clientData, contactNumber: e.target.value})}  />
+            <TextField className={classes.textFields} name="package" variant="outlined" label="Package" fullWidth value={clientData.package} onChange={(e) => setClientData({ ...clientData, package: e.target.value})} />
+            <TextField className={classes.textFields} name="dueDate" variant="outlined" label="Due Date" fullWidth value={clientData.dueDate} onChange={(e) => setClientData({ ...clientData, dueDate: e.target.value})}/>
+            <TextField className={classes.textFields} name="monthlyFee" variant="outlined" label="Monthly Fee" fullWidth value={clientData.monthlyFee} onChange={(e) => setClientData({ ...clientData, monthlyFee: e.target.value})}/>
+            <TextField className={classes.textFields} name="address" variant="outlined" label="Address" fullWidth value={clientData.address} onChange={(e) => setClientData({...clientData, address: e.target.value})} />
+          
+            <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth> Submit </Button>
+            <Button variant="contained" color="secondary" size="small" fullWidth> Clear </Button>
+      </form>
+    </Paper>
+    </>
+  )
 }
 
 export default Client;
--- src/components/Client/styles.js
+++ src/components/Client/styles.js
@@ -2,26 +2,27 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  root: {
-    '& .MuiTextField-root': {
-      margin: theme.spacing(1),
-    },
-  },
   Header: {
     display: 'flex',
-    margin: '3px 3px 3px 3px'
+    margin: '3px 3px 33px 3px',
+    fontFamily: 'Segoe UI'
+  },
+  textFields: {
+    marginBottom: '12px'
   },
   Select: {
     display: 'flex',
-    margin: '3px 3px 3px 3px'
+    margin: '3px 3px 33px 3px',
+    fontFamily: 'Segoe UI'
   },
   paper: {
     display: 'flex',
-    padding: theme.spacing(2),
+    padding: theme.spacing(6),
   },
   form: {
     display: 'flex',
     flexWrap: 'wrap',
+    flexDirection: 'column'
   },
 
   buttonSubmit: {
