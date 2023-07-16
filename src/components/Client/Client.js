0754cf483f281c6a1decf4eb77523943797d236f
Marvin Bendicio <marvin.bendicio.ipi@prulifeuk.com.ph>
2023-07-16 11:16:38 +0800

    awtatatasfdfasdfasd

--- src/components/Client/Client.js
+++ src/components/Client/Client.js
@@ -18,19 +18,16 @@ const Client = () => {
   const categories = useSelector(state => state.categories);
   const plans = useSelector(state => state.plan);
   
-  const [clientData, setClientData] = useState({ group:'', name: '', ipaddr: '', contactNumber: '', category: '', plan: '', planName: '', dueDate: '', monthlyFee: '', address: '' });
+  const [clientData, setClientData] = useState({ group:'', subloc:'', targloc:'',  name: '', ipaddr: '', contactNumber: '', category: '', plan: '', planName: '', dueDate: '', monthlyFee: '', firstPayment:'', address: '' });
   const [sublocData, setSublocData] = useState({ name: '', groupId: '' });
   const [category, setCategory] = useState('');
   const [plan, setPlan] = useState('');
   const [ggroup, setGgroup] = useState('');
-  const [sublocOnChangeValue, setSublocOnChangeValue] = useState('');
-  const [targetLocationOnChangeValue, setTargetlocationOnChangeValue] = useState('');
-  
-  const [targetlocation, setTargetlocation] = useState('');
+  const [ssubloc, setSsubloc] = useState('');
+  const [ttargloc, setTtargloc] = useState('');
   const [selectedCategId, setSelectedCategId] = useState('');
-
   const [sublocDataByGroupId, setSublocDataByGroupId] = useState([]);
-  const [targetlocDataBySublocId, setSublocDataBySublocId] = useState([]);
+  const [targetlocDataBySublocId, setTargetlocDataBySublocId] = useState([]);
 
   const dispatch = useDispatch();
   const classes = useStyles();
@@ -44,14 +41,10 @@ const Client = () => {
 
   }, [])
 
-  //group dropdown list
-  const handleOnchange = data => {
-    setGgroup(data);
-    let group = groups.filter(g => g.name == data);
-
-    setClientData({ ...clientData, group: group[0]._id});
+  const debugg = e => {
+    e.preventDefault();
+    console.log('clientData:: ', clientData);
   };
-
 
   const handleOnchangeCategory = data => {
 
@@ -64,13 +57,13 @@ const Client = () => {
     setSelectedCategId(category[0]._id);
   };
 
- const handleOnchangePlan = data => {
+  const handleOnchangePlan = data => {
   setPlan(data);
 
   let selectedPlan = plans.filter(p => p.plan == data);
   console.log(' selectedPlan[0].monthlyFee::: ',  selectedPlan[0].price);
      setClientData({ ...clientData, plan: selectedPlan[0]._id, planName: selectedPlan[0].plan, monthlyFee: selectedPlan[0].price});
- };
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -80,11 +73,6 @@ const Client = () => {
 
   const categoryOnClick = () => {
 
-  };
-
-  const debugg = e => {
-    e.preventDefault();
-    console.log('clientData:: ', clientData);
   };
 
   if (!user?.result?._id) {
@@ -99,88 +87,88 @@ const Client = () => {
 
   const LocationOnChange = groupId => {
 
-    setSublocOnChangeValue(groupId);
     setGgroup(groupId);
     setSublocData({ ...sublocData, groupId: groupId});
-
-     let holdSubloc = [];
+    let holdSubloc = [];
 
     if(groups){
       Object.keys(sublocations).forEach(i => {
         if(sublocations[i].groupId == groupId) {
           holdSubloc.push({ _id:  sublocations[i]._id, name: sublocations[i].name});
         }
-      })
+      });
 
       setSublocDataByGroupId(holdSubloc);
     }
+
+    setClientData({ ...clientData, group: groupId});
   };
 
   const SubLocationOnChange = sublocId => {
-    console.log('sublocIdsublocIdsublocId::: ', sublocId);
-     setTargetlocationOnChangeValue(sublocId);
-     setSublocDataBySublocId(sublocId);
-    // setGgroup(groupId);
-    // setSublocData({ ...sublocData, groupId: groupId});
+    setSsubloc(sublocId);
+     let holdTargloc = [];
 
-    //  let holdSubloc = [];
-
-    // if(groups){
-    //   Object.keys(sublocations).forEach(i => {
-    //     if(sublocations[i].groupId == groupId) {
-    //       holdSubloc.push({ _id:  sublocations[i]._id, name: sublocations[i].name});
-    //     }
-    //   })
-
-    //   setSublocDataByGroupId(holdSubloc);
-    // }
+    if(groups){
+      Object.keys(targetlocations).forEach(i => {
+        if(targetlocations[i].sublocId == sublocId) {
+          holdTargloc.push({ _id:  targetlocations[i]._id, name: targetlocations[i].name});
+        }
+      })
+      setTargetlocDataBySublocId(holdTargloc);
+      setClientData({ ...clientData, subloc: sublocId});
+    }
   };
 
-  const targetLocationOnChange = targetLocId => {
-    setTargetlocation(targetLocId);
+  const TargetLocationOnChange = data => {
+    setTtargloc(data);
+    setClientData({ ...clientData, targloc: data});
   };
 
   return (     
     <Container component="main" maxWidth="xs">     
       <Paper className={classes.paper} elevation={9}>
-        <form onSubmit={handleSubmit}>
-          <FormControl>
+        <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit}>
 
+          <FormControl>
             <FormLabel>Location</FormLabel>
             <Select className={classes.Select} fullWidth value={ggroup} onChange={e => LocationOnChange(e.target.value)}>
               {groups.map((data) => (
                 <MenuItem key={data._id} value={data._id}>{data.name}</MenuItem>
               ))}
             </Select>
+          </FormControl>
 
+
+          <FormControl>
             <FormLabel>Sub Location</FormLabel>
-            <Select className={classes.Select} fullWidth value={sublocOnChangeValue} onChange={e => SubLocationOnChange(e.target.value)}>
+            <Select className={classes.Select} fullWidth value={ssubloc} onChange={e => SubLocationOnChange(e.target.value)}>
               {sublocDataByGroupId.map((data) => (
                 <MenuItem key={data._id} value={data._id}>{data.name}</MenuItem>
               ))}
             </Select>
+          </FormControl>
 
+          <FormControl>
             {/* targetloc */}
             <FormLabel>Target Location</FormLabel>
-            <Select className={classes.Select} fullWidth value={targetlocation} onChange={e => targetLocationOnChange(e.target.value)}>
-
-              {targetlocDataBySublocId.map((data) => (
-                <MenuItem key={data._id} value={data._id}>{data.name}</MenuItem>
+            <Select className={classes.Select} fullWidth value={ttargloc} onChange={e => TargetLocationOnChange(e.target.value)}>
+              {targetlocDataBySublocId.map(tl => (
+                <MenuItem key={tl._id} value={tl._id}>{tl.name}</MenuItem>
               ))}
             </Select>
-
-            <TextField required className={classes.textFields} name="name" variant="outlined" label="Registered name" fullWidth value={clientData.name} onChange={(e) => setClientData({...clientData, name: e.target.value})} />
-            <TextField required className={classes.textFields} name="address" variant="outlined" label="Address" fullWidth value={clientData.address} onChange={(e) => setClientData({...clientData, address: e.target.value})} />
-            <TextField required className={classes.textFields} name="contactNumber" variant="outlined" label="Contact Number" fullWidth value={clientData.contactNumber} onChange={(e) => setClientData({...clientData, contactNumber: e.target.value})}  />
-           
+            </FormControl>
+            
+            <TextField id="tfOne"  required className={classes.textFields} name="name" variant="standard" label="Registered name" fullWidth value={clientData.name} onChange={(e) => setClientData({...clientData, name: e.target.value})} />
+            <TextField id="tfTwo"  required className={classes.textFields} name="address" variant="outlined" label="Address" fullWidth value={clientData.address} onChange={(e) => setClientData({...clientData, address: e.target.value})} />
+            <TextField id="tfThree"  required className={classes.textFields} name="contactNumber" variant="outlined" label="Contact Number" fullWidth value={clientData.contactNumber} onChange={(e) => setClientData({...clientData, contactNumber: e.target.value})}  />
+            
+            <FormControl>
             <FormLabel>Product</FormLabel>
             <Select                      
                 style={{paddingBottom: '.3em'}} 
-                labelId="demo-simple-select-standard-label" 
-                id="demo-simple-select-standard" 
                 className={classes.Select}                             
                 value={category} 
-                visible={false}
+                visible="false"
                 onClick={categoryOnClick}
                 onChange={e => handleOnchangeCategory(e.target.value)}>
 
@@ -188,7 +176,9 @@ const Client = () => {
                   <MenuItem key={data._id} value={data.category}>{data.category}</MenuItem>
                 ))}
             </Select>   
-
+            </FormControl>
+            
+            <FormControl>
             <FormLabel>Plan</FormLabel>
             <Select                      
                 style={{paddingBottom: '.3em'}} 
@@ -202,6 +192,7 @@ const Client = () => {
                     }                          
                 })}
             </Select>    
+            </FormControl>
 
             <FormLabel id="demo-radio-buttons-group-label">Due Date</FormLabel>
             <RadioGroup aria-labelledby="demo-radio-buttons-group-label" defaultValue="15th" name="radio-buttons-group">
@@ -211,11 +202,8 @@ const Client = () => {
       
             <TextField required className={classes.textFields} name="ipaddr" variant="outlined" label="IP Address" fullWidth value={clientData.ipaddr} onChange={(e) => setClientData({ ...clientData, ipaddr: e.target.value})}/>
             <TextField required className={classes.textFields} name="monthlyFee" variant="outlined" label="Monthly Fee" fullWidth value={clientData.monthlyFee} onChange={(e) => setClientData({ ...clientData, monthlyFee: e.target.value})}/>
-          
             <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth> Submit </Button>
-            {/* <Button variant="contained" color="secondary" size="small" fullWidth onClick={debugg}> Clear </Button> */}
-
-          </FormControl>
+            <Button variant="contained" color="secondary" size="small" fullWidth onClick={debugg}> Clear </Button>
         </form>
     </Paper>
     </Container>
